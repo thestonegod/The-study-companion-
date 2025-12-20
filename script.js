61deec1aebe911bcdf0321f317fdbff48304b029
thestonegod <elshaddaisandstone@gmail.com>
2025-12-19 23:34:49 -0800

    Final HTML structure + Quotes in JS

---
+++ script.js
@@ -0,0 +1,34 @@
+//JS waits until HTML exists
+document.addEventListener("DOMContentLoaded"),function() {
+    //List of quotes
+    const entryQuotes = [
+      "Don't you wanna be smart?",
+      "One session everyday is a ton of knowledge.",
+      "Do what makes you feel you.",
+      "Procastination is a sign of indiscipline.",
+      "Focus is a deliberate decision.",
+      "knowledge never gets old"
+    ];
+
+    const focusQuotes = [
+        "Attention is power.",
+        "Deep work builds sharp minds.",
+        "Repeatition isn't failure. It's quiet mastery.",
+        "Stay grounded.",
+        "Dont stop at challenges,stop at solutions."
+    ]
+
+    const closureQuotes = [
+        "You logged in to a self-upgrade system today.",
+        "Progress was made!",
+        "Baby steps train you for the big leaps.",
+        "Successful at heart",
+        "I made this with people like you in mind",
+        "Reflect. Reset. Repeat."
+    ]
+
+    function getRandomQuote(quotesArray) {
+        const randomIndex = Math.floor(Math.random()* quotesArray.length);
+        return quotesArray[randomIndex];
+    }
+}
